774c9d9740817c1ba489a639cce4d412445ca036
xiangbaoyan <271190187@qq.com>
2014-03-07 08:11:06 +0800

    10

--- admin/manaUsers.php
+++ admin/manaUsers.php
@@ -1,13 +1,3 @@
-<?php
-    define("IN_TG",true);
-    require $_SERVER['DOCUMENT_ROOT']."/functions/mysqlFun.php";
-
-    _connect();
-    _select_db();
-    _set_names();
-    $sql = "select tg_username,tg_level,tg_reg_time from tg_user";
-    $result = _query($sql);
-?>
 <div style="margin-top: 50px">
     <ul class="page-breadcrumb breadcrumb">
         <li>
@@ -24,12 +14,27 @@
             </a>
         </li>
     </ul>
+
+    <?php
+    define("IN_TG",true);
+    require $_SERVER['DOCUMENT_ROOT']."/functions/mysqlFun.php";
+
+    _connect();
+    _select_db();
+    _set_names();
+    $sql = "select tg_username,tg_level,tg_reg_time from tg_user";
+    $result = _query($sql);
+
+    while($row = _fetch_array_list($result)){
+        ?>
     <div class="portlet box blue">
         <div class="portlet-title">
             <div class="caption">
-                <i class="fa fa-user"></i>Xiangbaoyan
+                <i class="fa fa-user"></i><?php
+                    echo $row['tg_username'];
+                ?>
             </div>
-            <div class="tools">
+            <div class="tools" style="padding-bottom: 5px">
                 <button class="btn red-stripe">
                     <i class="fa fa-eraser"></i>
                     删除用户</button>
@@ -40,22 +45,32 @@
                 <tbody>
                 <tr>
                     <td>
-                       用户级别
+                        用户级别
                     </td>
                     <td>
-                        管理员
+                       <?php 
+                           echo levelToName($row['tg_level']);
+                       ?>
                     </td>
                 </tr>
                 <tr>
-                <td>
-                    注册日期
-                </td>
-                <td>
-                    管理员
-                </td>
+                    <td>
+                        注册日期
+                    </td>
+                    <td>
+                        <?php
+                        echo $row['tg_reg_time'];
+                        ?>
+                    </td>
                 </tr>
                 </tbody>
             </table>
         </div>
     </div>
 </div>
+
+
+    <?php
+    }
+    ?>
+
