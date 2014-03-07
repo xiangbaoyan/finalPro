da263eb6387a766cae5799f95c55518cc58f053f
xiangbaoyan <271190187@qq.com>
2014-03-07 12:45:28 +0800

    16

--- functions/insertUsers.php
+++ functions/insertUsers.php
@@ -1,18 +1,20 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT']."/functions/commonFun.php";
-require $_SERVER['DOCUMENT_ROOT']."/functions/mysqlFun.php";
+define("IN_TG", true);
+require $_SERVER['DOCUMENT_ROOT'] . "/functions/commonFun.php";
+require $_SERVER['DOCUMENT_ROOT'] . "/functions/mysqlFun.php";
 
-    function insertUsers(){
+function insertUsers()
+{
 
-        $arr['userDir'] = "/users/".substr(_sha1_uniqid(),0,10);
-        mkdir($_SERVER['DOCUMENT_ROOT'].$arr['userDir']);
-        $arr['userName'] = "test".substr(_sha1_uniqid(),0,4);
+    $arr['userDir'] = "/users/" . substr(_sha1_uniqid(), 0, 10);
+    mkdir($_SERVER['DOCUMENT_ROOT'] . $arr['userDir']);
+    $arr['userName'] = "test" . substr(_sha1_uniqid(), 0, 4);
 
-        $arr['password'] = substr(_sha1_uniqid(),0,9);
-         $arr['uniqid'] = _sha1_uniqid();
-        $sexArr = ["m","w"];
-        $arr['sex'] = $sexArr[array_rand($sexArr)];
-        $sql = "insert into tg_user ( tg_userDir,
+    $arr['password'] = substr(_sha1_uniqid(), 0, 9);
+    $arr['uniqid'] = _sha1_uniqid();
+    $sexArr = ["m", "w"];
+    $arr['sex'] = $sexArr[array_rand($sexArr)];
+    $sql = "insert into tg_user ( tg_userDir,
                                       tg_username,
                                       tg_password,
                                       tg_uniqid,
@@ -26,11 +28,17 @@ require $_SERVER['DOCUMENT_ROOT']."/functions/mysqlFun.php";
                                       '{$arr['sex']}'
                               )";
 
-        _query($sql);
-    }
+    _query($sql);
+}
 
 
-    for($i=0;$i<20;$i++){
-        insertUsers();
-    }
+_connect();
+_select_db();
+_set_names();
+
+for ($i = 0; $i < 20; $i++) {
+    insertUsers();
+}
+
+_close();
 ?>
