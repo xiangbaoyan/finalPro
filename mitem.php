7b060f503301884d929b80f9c027ab52cdca7f34
xiangbaoyan <271190187@qq.com>
2014-03-08 21:25:30 +0800

    232

---
+++ mitem.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>商品详情</title>
+    <meta name="author" content="www.dabinzhou.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="format-detection" content="telephone=no">
+
+    <link rel="stylesheet" type="text/css"
+          href="/css/mitem.css">
+    <style type="text/css"></style>
+</head>
+<body id="body">
+<header>
+    <div class="header">
+        <a id="tuanDetailBack" href="javascript:pageBack()" class="back"><span></span>返回</a>
+
+        <h2 class="fw-normal">
+            【渤海国际】丽都国际美容整形 </h2>
+
+        <div class="btn-area-rt">
+            <a href="/m.html" class="btn-sear-rt"></a>
+        </div>
+    </div>
+</header>
+<div class="ct">
+    <div class="view-img">
+        <div id="spinner" class="spinner" style="left:50%;margin:-22px -15px;"></div>
+        <div style="min-height:140px;max-height:220px;" class="photo txt-cent" id="div_img">
+            <img width="220" src="http://s1.dabinzhou.com/public/attachment/201403/05/09/531678eec1ad6_287x179.jpg"
+                 >
+        </div>
+    </div>
+    <div class="price bg-type">
+        <p class="price-p1 posi-re">
+            <strong class="red"><span>￥38</span></strong>&nbsp;&nbsp;
+            <del class="ft14">￥1280</del>
+        </p>
+        <div style="margin-right: 60px;margin-top: -9px;margin-bottom: 7px;">
+            <span id="startOrder">
+            <a style="text-align: center;display: block;width: 100%;" class="btn-type" >立即购买</a>
+            </span>
+        </div>
+        <p></p>
+
+        <p class="price-p2 posi-re">
+            <span class="purchaser ft14"><span></span><strong>8</strong>人已购买</span>
+                <span id="time" class="time">
+											剩余3天以上									</span>
+        </p>
+    </div>
+    <input type="hidden" value="7579740" id="counterTime">
+
+    <p class="p-txt txt3">仅售38元，享受价值1280元的尊宠贵宾卡一张。面部水上芭蕾3次+顶级…</p>
+
+    <div class="pd10">
+        <ul class="lst-type3">
+            <li class="lt-li">
+                <a class="lt-a" href="#">本单详情 <span class="li-arrow"></span></a>
+            </li>
+            <li class="lt-li">
+                <a class="lt-a" href="#"> 商家简介<span class="li-arrow"></span></a>
+            </li>
+        </ul>
+    </div>
+</div>
+<footer>
+    <div class="to-top"><a href="#top"><span></span>回顶部</a></div>
+    <div class="footer">
+        <div>
+            <a href="/muser-login.php" style="padding-right: 10px;">登录</a><span class="new-bar2">|</span><a
+                href="/muser-register.php" style="padding-left: 10px;">注册</a>
+        </div>
+        <a href="/m.html" style="padding-right: 10px;">触屏版</a> | <a
+            href="#" style="padding-left: 10px;">电脑版</a>
+
+        <div class="gray">Copyright © 2013 Xiangabaoyan <a target="_blank" href="#">鲁ICP备13022210号</a>
+        </div>
+    </div>
+</footer>
+</body><script>function backHis(){
+    history.back();
+}function showOrHide(id) {
+    var tardiv = $("#"+id);
+    if(tardiv.css("display")=="none"){
+        tardiv.css("display","block");
+    }else{
+        tardiv.css("display","none");
+    }
+}</script>
+</html>
