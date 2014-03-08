b82c70eb89181e4b752356d0d93c1d11816f5f4a
xiangbaoyan <271190187@qq.com>
2014-03-08 21:03:40 +0800

    23

--- mcart.php
+++ mcart.php
@@ -1,63 +1,63 @@
-<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
-		<title></title>
-	<meta name="author" content="www.dabinzhou.com">	
-    		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-        <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>购物车</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="format-detection" content="telephone=no">
 
-<link rel="stylesheet" type="text/css" href="/css/mcart4.css"  />
-<script type="text/javascript" src="/js/mcart5.js"  charset="utf-8"></script>
-<script type="text/javascript">
-var APP_ROOT = '';
-var CART_URL = 'mcart.php';
-var CART_CHECK_URL = 'muser-login.php';
-var LOADER_IMG = '/images/lazy_loading.gif';
-var ERROR_IMG = '/images/image_err.gif';
-</script>
-</head><body id="body">
-	<header>
-	<div class="header">
-	<a id="tuanDetailBack" href='javascript:pageBack()' class="back"><span></span>返回</a>
+    <link rel="stylesheet" type="text/css" href="/css/mcart4.css"/>
+    <script type="text/javascript" src="/js/mcart5.js" charset="utf-8"></script>
+</head>
+<body id="body">
+<header>
+    <div class="header">
+        <a id="tuanDetailBack" href='javascript:pageBack()' class="back"><span></span>返回</a>
 
-	<h2 class="fw-normal">
-	 购物车	</h2>
-	<div class="btn-area-rt">
-    	<a href="m.php"  class="btn-sear-rt"></a>
+        <h2 class="fw-normal">
+            购物车 </h2>
+
+        <div class="btn-area-rt">
+            <a href="m.php" class="btn-sear-rt"></a>
+        </div>
     </div>
-	</div>
 </header>
 <div class="t-virtual t-entity">
-	
-             <div class="mc">
-             					<div class="blank"></div>
-					<div class="nogoodsbg">
-						<div class="nogoodsimg"><img src="/images/nogoods.jpg" ></div>
-						<div class="nogoodstxt"><p class="empty">您的购物车还是空的。</p>
-						<p class="color_blue">您还没有添加任何商品。马上去 [ <a href="m.php" >挑选商品</a> ]，或者去 [ <a href="user-login.php" >我的收藏夹</a> ] 看看。
-							<br></p><br><br><br><br></div> 
-						<div class="blank"></div>
-					</div>	
-                    
+
+    <div class="mc">
+        <div class="blank"></div>
+        <div class="nogoodsbg">
+            <div class="nogoodsimg"><img src="/images/nogoods.jpg"></div>
+            <div class="nogoodstxt"><p class="empty">您的购物车还是空的。</p>
+
+                <p class="color_blue">您还没有添加任何商品。马上去 [ <a href="m.php">挑选商品</a> ]，或者去 [ <a
+                        href="user-login.php">我的收藏夹</a> ] 看看。
+                    <br></p><br><br><br><br></div>
+            <div class="blank"></div>
+        </div>
+
     </div>
-	 </div>
+</div>
 
 <div style="margin-top:-15px"></div>
-<script type="text/javascript" src="/js/mcart6.js" ></script>
-	<footer>
-	<div class="to-top"><a href="#top"><span></span>回顶部</a></div>
-	
-	<div class="footer">
-    	<div>
-                        <a href="muser-login.php"  style="padding-right: 10px;">登录</a><span class="new-bar2">|</span><a href="muser-register.php"  style="padding-left: 10px;">注册</a>
-                			    		    		
-    		    	</div>
-    	<a href="m.php"  style="padding-right: 10px;">触屏版</a> | <a href="index.htm"  style="padding-left: 10px;">电脑版</a>
-    	<div class="gray">Copyright &copy; 2013 dabinzhou.com <a  target="_blank" href="javascript:if(confirm('http://www.miitbeian.gov.cn/  \n\n���ļ��޷��� Teleport Ultra ����, ��Ϊ ����һ�����·���ⲿ������Ϊ������ʼ��ַ�ĵ�ַ��  \n\n�����ڷ������ϴ���?'))window.location='http://www.miitbeian.gov.cn/'" >鲁ICP备13022210号</a></div>
-	</div>
+<script type="text/javascript" src="/js/mcart6.js"></script>
+<footer>
+    <div class="to-top"><a href="#top"><span></span>回顶部</a></div>
+
+    <div class="footer">
+        <div>
+            <a href="muser-login.php" style="padding-right: 10px;">登录</a><span class="new-bar2">|</span><a
+                href="muser-register.php" style="padding-left: 10px;">注册</a>
+
+        </div>
+        <a href="m.php" style="padding-right: 10px;">触屏版</a> | <a href="index.htm" style="padding-left: 10px;">电脑版</a>
+
+        <div class="gray">Copyright &copy; 2013 dabinzhou.com <a target="_blank"
+                                                                 href="javascript:if(confirm('http://www.miitbeian.gov.cn/  \n\n���ļ��޷��� Teleport Ultra ����, ��Ϊ ����һ�����·���ⲿ������Ϊ�����ʼ��ַ�ĵ�ַ��  \n\n�����ڷ������ϴ���?'))window.location='http://www.miitbeian.gov.cn/'">鲁ICP备13022210号</a>
+        </div>
+    </div>
 </footer>
 </body>
 </html>
